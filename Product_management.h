17b73d3fd4ef47df060d8fcca9d3193c36ee88b0
ImmortalA <tranducanhmail@gmail.com>
2021-04-18 21:18:52 +0700

    Update

--- Product_management.h
+++ Product_management.h
@@ -8,25 +8,35 @@ private:
     /* data */
     int sl_;
     string name_;
+    string ID_;
+    int amount_;
+    int price_;
+    double percent_;
+    string start_day_, end_date_;
+    int discount_price_;
 
 public:
-    Product(string name)
-    {
-        this->name_ = name;
-    }
+    Product();
+
     void IsProduct();
-    void SetName(string name);
-    void SetID(int id);
-    void SetAmount(int amount);
-    void SetPrice(int price);
-    void SetDiscount(double percent, string start_date, string end_date);
+
+    void GetName();
+    void SetName(string name_);
+    void GetID();
+    void SetID(string ID_);
+    void GetAmount();
+    void SetAmount(int amount_);
+    void GetPrice();
+    void SetPrice(int price_);
+    void GetDiscount();
+    void SetDiscount(double percent_, string start_date_, string end_date_);
+
     bool Status();
     ~Product() {}
 };
 
-void Product::SetName(string name)
+void Product::GetName()
 {
-    cout << "Enter product name:" << endl;
     cin.ignore(256, '\n');
-    getline(cin, name);
+    getline(cin, name_);
 }
