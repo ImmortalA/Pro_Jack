f533d93a20f85e16ad97d2e9f1e1cd1f779ce098
ImmortalA <tranducanhmail@gmail.com>
2021-04-21 19:03:52 +0700

    Update Mouse

--- Mouse.h
+++ Mouse.h
@@ -3,51 +3,60 @@
 class Mouse
 {
 private:
-    string name_;
+    string brand_;
     string type_;
     string color_;
 
 public:
-    Mouse() {}
-    ~Mouse();
-    Mouse(string name)
-    {
-        this->name_ = name;
-    }
-    string getName()
-    {
-        return this->name_;
-    }
-    void setName(string name)
-    {
-        this->name_ = name;
-    }
-    string getType()
-    {
-        return this->type_;
-    }
-    void setType()
-    {
-        int op;
-        cout << "Your mouse type: \n";
-        cout << "1.Bluetooth        2.Wire\n";
-        cout << "Your selection: ";
-        cin >> op;
-        this->type_ = op ? "Bluetooth" : "Wire";
-    }
-    string getColor()
-    {
-        return this->color_;
-    }
-    void setColor()
-    {
-        cout << "Enter your " << this->name_ << " mouse color: ";
-        cin >> this->color_;
-    }
-    void show()
-    {
-        cout << "Name: " << this->name_ << endl;
-        cout << "Type: " << this->type_ << endl;
-        cout << "Color: " << this->color_ << endl;
-    }
+    Mouse();
+    Mouse(string brand_);
+
+    void SetBrand();
+    string GetBrand();
+
+    void SetType();
+    string GetType();
+
+    void SetColor();
+    string GetColor();
+
+    void Show();
 };
+
+Mouse::Mouse(string brand_)
+{
+    this->brand_ = brand_;
+}
+void Mouse::SetBrand()
+{
+    cin.ignore(256, '\n');
+    getline(cin, brand_);
+}
+string Mouse::GetBrand()
+{
+    return this->brand_;
+}
+void Mouse::SetType()
+{
+    cin.ignore(256, '\n');
+    getline(cin, type_);
+}
+string Mouse::GetType()
+{
+    return this->color_;
+}
+void Mouse::SetColor()
+{
+    cin.ignore(256, '\n');
+    getline(cin, color_);
+}
+string GetColor()
+{
+
+}
+void Mouse::Show()
+{
+    cout << "Name: " << this->brand_ << endl;
+    cout << "Type: " << this->type_ << endl;
+    cout << "Color: " << this->color_ << endl;
+}
